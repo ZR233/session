5a584d5220bbb3dc87a2006d3760adf08e753186
zrufo <zrufo747@outlook.com>
2019-07-12 18:41:13 +0800

    init

--- adapter/redis.go
+++ adapter/redis.go
@@ -3,3 +3,48 @@
 @Author : zr
 */
 package adapter
+
+import (
+	"github.com/ZR233/session/session"
+	"github.com/go-redis/redis"
+	"time"
+)
+
+type Redis struct {
+	db *redis.Client
+}
+
+func (r Redis) CreateTokenMap(token string, channel string) error {
+	panic("implement me")
+}
+
+func (Redis) TokenMapTokenExpireAt(token string, expireAt time.Time) error {
+	panic("implement me")
+}
+
+func (Redis) UserIdTokenListAppendToken(userId string, token string, expireAt time.Time) error {
+	panic("implement me")
+}
+
+func (Redis) FindByToken(token string) (*session.Session, error) {
+	panic("implement me")
+}
+
+func (Redis) UpdateTokenMapSetUserId(token string, userId string) error {
+	panic("implement me")
+}
+
+func (Redis) UpdateTokenMapSetJsonField(token string, jsonField string) error {
+	panic("implement me")
+}
+
+func (Redis) FindTokenByUserId(token string) ([]string, error) {
+	panic("implement me")
+}
+
+func NewRedis(client *redis.Client) Redis {
+	a := Redis{
+		client,
+	}
+	return a
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ZR233/session
 
 go 1.12
+
+require github.com/go-redis/redis v6.15.3+incompatible
